dcddcafaf631a57e6214ae711b6d88b5aec27338
BogdanMalynochko <bogdanmalynochko@gmail.com>
2021-10-09 18:04:17 +0300

    mobile v2

--- app/js/common.js
+++ app/js/common.js
@@ -8,3 +8,14 @@ $(document).ready(function() {
 		fixedContentPos: false
 	});
 });
+
+window.addEventListener("scroll", function(){
+	var header = document.querySelector("header");
+	header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+var button = document.querySelector('.mobile-button');
+var menu = document.querySelector('#header-mobile');
+button.onclick = function(){
+  menu.classList.toggle('active')
+}
